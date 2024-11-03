bb0c29e64a499179710e9ae7040dc5aba7c0ecdd
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-03 12:46:08 +0800

    Update onlinefood.py

--- onlinefood.py
+++ onlinefood.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+file_path = 'https://raw.githubusercontent.com/nuruldini/jie43203/refs/heads/main/onlinefoods.csv'
+
+df = pd.read_csv(file_path)
+
+st.write(df)
+
 
