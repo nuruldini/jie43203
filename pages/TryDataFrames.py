3dfe128846782b129111602db9cd2476c0d43b80
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-05 08:28:36 +0800

    Update TryDataFrames.py

--- pages/TryDataFrames.py
+++ pages/TryDataFrames.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import pandas as pd
+
 df = pd.DataFrame(
     [
         {"command": "st.selectbox", "rating": 4, "is_widget": True},
@@ -6,7 +9,4 @@ df = pd.DataFrame(
     ]
 )
 
-edited_df = st.data_editor(df) # 👈 An editable dataframe
-
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+st.dataframe(df, use_container_width=True)
