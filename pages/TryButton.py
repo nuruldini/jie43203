fc91e4af246fe6a872f55674c1cf8ced80e660db
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-05 08:20:27 +0800

    Update TryButton.py

--- pages/TryButton.py
+++ pages/TryButton.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
+my_data_element = st.line_chart(df)
+
+for tick in range(10):
+    time.sleep(.5)
+    add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+    my_data_element.add_rows(add_df)
+
+st.button("Regenerate")
 
